ff5dc38438408b8edadfb36d5a681673d7f234d7
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-08-24 15:04:24 -0300

    Desenvolvi a paginação e o campo de busca com jquery no projeto Ambiente Escolar Curso

--- AmbienteEscolarCurso/wwwroot/js/site.js
+++ AmbienteEscolarCurso/wwwroot/js/site.js
@@ -1,1 +1,21 @@
-﻿
+﻿$(document).ready(function () {
+    // Aplica DataTables em todas as tabelas com a classe 'datatable'
+    $('#registros').DataTable({
+        language: {
+            sLengthMenu: "Mostrar _MENU_ registros por página",
+            sZeroRecords: "Nenhum registro encontrado",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar",
+            oPaginate: {
+                sNext: "Próximo",
+                sPrevious: "Anterior",
+                sFirst: "Primeiro",
+                sLast: "Último"
+            },
+            oAria: {
+                sSortAscending: ": Ordenar colunas de forma ascendente",
+                sSortDescending: ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
