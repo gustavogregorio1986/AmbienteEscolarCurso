83c8efc322d277d5e5fafe576133f46265210719
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-08-24 16:49:57 -0300

    Desenvolver a tela do cadastro no projeto

--- AmbienteEscolarCurso/Controllers/AlunoController.cs
+++ AmbienteEscolarCurso/Controllers/AlunoController.cs
@@ -1,19 +1,25 @@
-﻿using AmbienteEscolarCurso.Services.Aluno;
+﻿using AmbienteEscolarCurso.Models;
+using AmbienteEscolarCurso.Services.Aluno;
+using AmbienteEscolarCurso.Services.Turma;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AmbienteEscolarCurso.Controllers
 {
     public class AlunoController : Controller
     {
         private readonly IAlunoInterface _alunoInterface;
+        private readonly ITurmaInterface _turmaInterface;
 
-        public AlunoController(IAlunoInterface alunoInterface)
+        public AlunoController(IAlunoInterface alunoInterface, ITurmaInterface turmaInterface)
         {
             _alunoInterface = alunoInterface;
+            _turmaInterface = turmaInterface;
         }
 
         public IActionResult Index()
         {
+            BuscarTurmas();
             return View();
         }
 
@@ -25,11 +31,34 @@ namespace AmbienteEscolarCurso.Controllers
         }
 
         [HttpGet]
+        public IActionResult CadastrarAluno()
+        {
+            BuscarTurmas();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarAluno(AlunoModel alunoModel)
+        {
+            BuscarTurmas();
+            return View();
+        }
+
+        [HttpGet]
         [Route("/Aluno/AlunosDaTurma/{idTurma}")]
         public IActionResult AlunosDaTurma(int idTurma)
         {
             var alunos = _alunoInterface.BuscarAlunosPorTurma(idTurma);
             return Json(new { dados = alunos });
+        }
+
+        public void BuscarTurmas()
+        {
+            var turmas = _turmaInterface.ListarTurmas();
+
+            var listaTurma = new SelectList(turmas, "Id","Descricao");
+
+            ViewBag.Turmas = listaTurma;
         }
     }
 }
